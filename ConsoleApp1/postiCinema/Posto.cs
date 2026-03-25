461d6083d2bc9d973840a38db362bbc0063107f0
14529839 <14529839@itisrossi.vi.it>
2026-03-25 10:02:20 +0100

    Inserite le classi

---
+++ ConsoleApp1/postiCinema/Posto.cs
@@ -0,0 +1,23 @@
+﻿public class Posto
+{
+    public enum Tipo { standard, vip }
+    public enum Stato { libero, occupato, nd } 
+
+    public char Fila { get; private set; }
+    public int Numero { get; private set; }
+    public Tipo TipoPosto { get; private set; }
+    public Stato StatoAttuale { get; set; } 
+
+    public Posto(char fila, int numero, Tipo tipo, Stato stato)
+    {
+        this.Fila = fila;
+        this.Numero = numero;
+        this.TipoPosto = tipo;
+        this.StatoAttuale = stato;
+    }
+
+    public override string ToString()
+    {
+        return $"Fila {Fila}, Numero {Numero} [{TipoPosto}] - Stato: {StatoAttuale}";
+    }
+}
---
+++ ConsoleApp1/postiCinema/Sala.cs
@@ -0,0 +1,33 @@
+﻿public class Sala
+{
+    public string Nome { get; private set; }
+    private List<Posto> posti;
+    private int capienza;
+    private int nCorridoi;
+    private List<string> addetti;
+
+    public Sala(string nome, int capienza, int nCorridoi)
+    {
+        this.Nome = nome;
+        this.capienza = capienza;
+        this.nCorridoi = nCorridoi;
+        this.posti = new List<Posto>();
+        this.addetti = new List<string>();
+    }
+
+    public void AggiungiPosto(Posto p) { if (posti.Count < capienza) posti.Add(p); }
+    public void AggiungiAddetto(string a) => addetti.Add(a);
+    
+    public int PostiLiberi() 
+    {
+        return posti.Count(p => p.StatoAttuale == Posto.Stato.libero);
+    }
+    
+    public int PostiOccupati() => posti.Count(p => p.StatoAttuale == Posto.Stato.occupato);
+    public bool IsVuota() => posti.All(p => p.StatoAttuale == Posto.Stato.libero);
+
+    public override string ToString()
+    {
+        return $"Sala: {Nome} | Capienza: {capienza} | Corridoi: {nCorridoi} | Addetti: {string.Join(", ", addetti)}";
+    }
+}
